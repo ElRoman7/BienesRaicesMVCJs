a212a9b70f8c84f50e44f6737a4382d4166902e1
ElRoman7 <sergio.romam@outlook.com>
2024-08-11 13:27:49 -0600

    Validaciones Formulario Propiedad

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -9,6 +9,16 @@ router.get('/mis-propiedades', admin)
 router.get('/propiedades/crear', crear)
 router.post('/propiedades/crear', 
     body('titulo').notEmpty().withMessage('El titulo del Anuncio es obligatorio'),
+    body('descripcion').notEmpty().withMessage('La descripción no puedde ir vacía')
+        .isLength({max: 200}).withMessage('La descripción es muy larga'),
+    body('categoria').isNumeric().withMessage('Selecciona una categoría'),
+    body('precio').isNumeric().withMessage('Selecciona un Rango de Precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona la cantidad de baños'),
+    body('lat').isNumeric().withMessage('Ubica la propiedad en el mapa'),
+
+
     guardar
 );
 
