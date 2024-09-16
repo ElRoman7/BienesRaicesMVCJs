1568a4f6af660e6342f133bed5e6d94092b884be
ElRoman7 <sergio.romam@outlook.com>
2024-09-16 12:10:57 -0600

    feat: categorías

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,9 +2,58 @@ import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 const inicio = async (req, res) => {
 
-    const [ categorias, precios ] = await Promise.all([
+    const [ categorias, precios, casas, departamentos ] = await Promise.all([
         Categoria.findAll({raw : true}),
-        Precio.findAll({raw : true})
+        Precio.findAll({raw : true}),
+        Propiedad.findAll(
+            {
+                limit: 3,
+                where:{
+                    categoriaId : 1
+                },
+                include :[
+                    {
+                        model: Precio,
+                        as: 'precio'
+                    }
+                ],
+                order: [['createdAt','DESC']]
+            }
+        ),
+        Propiedad.findAll(
+            {
+                limit: 3,
+                where:{
+                    categoriaId : 2
+                },
+                include :[
+                    {
+                        model: Precio,
+                        as: 'precio'
+                    }
+                ],
+                order: [
+                    ['createdAt','DESC']
+                ]
+            }
+        ),
+        Propiedad.findAll(
+            {
+                limit: 3,
+                where:{
+                    categoriaId : 3
+                },
+                include :[
+                    {
+                        model: Precio,
+                        as: 'precio'
+                    }
+                ],
+                order: [
+                    ['createdAt','DESC']
+                ]
+            }
+        )
     ])
 
     console.log(categorias);
@@ -13,13 +62,38 @@ const inicio = async (req, res) => {
     res.render('inicio',{
         pagina: 'Inicio',
         categorias,
-        precios
+        precios,
+        casas, 
+        departamentos
     })
 }
 
 
-const categoria = (req, res) => {
+const categoria = async(req, res) => {
     
+    const { id } = req.params
+
+    // Comprobar que la categoría exista
+    const categoria = await Categoria.findByPk(id)
+    if(!categoria){
+        return res.redirect('/404');
+    }
+    // Obtener las propiedades de la categoría    
+    const propiedades = await Propiedad.findAll({
+        where:{
+            categoriaId: id
+        },
+        include:[
+            {
+                model: Precio, as: 'precio'
+            }
+        ]
+    });
+
+    res.render('categoria', {
+        pagina: `${categoria.nombre}s en Venta `,
+        propiedades,
+    })
 }
 
 const noEncontrado = (req, res) => {
