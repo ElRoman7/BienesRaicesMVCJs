30c1b6b510ccec454b5ab55f6758d5f7def11aeb
ElRoman7 <sergio.romam@outlook.com>
2024-09-19 08:17:24 -0600

    fix: carpeta uploads

--- middleware/subirImagen.js
+++ middleware/subirImagen.js
@@ -1,16 +1,23 @@
 import multer from "multer";
 import path from 'path';
-import { generarId } from '../helpers/tokens.js'
+import { generarId } from '../helpers/tokens.js';
+import fs from 'fs';
 
-const storage = multer.diskStorage({ //cb=callback
-    destination: function(req, file, cb){
-        cb(null,'./public/uploads/')
+// Verificar si la carpeta "uploads" existe, y crearla si no
+const uploadsPath = './public/uploads/';
+if (!fs.existsSync(uploadsPath)) {
+    fs.mkdirSync(uploadsPath, { recursive: true }); // Crea la carpeta y subcarpetas si es necesario
+}
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, uploadsPath); // Usamos la ruta verificada
     },
-    filename: function(req, file, cb){
-        cb(null, generarId() + path.extname(file.originalname))
+    filename: function(req, file, cb) {
+        cb(null, generarId() + path.extname(file.originalname));
     }
-})
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
-export default upload
+export default upload;
